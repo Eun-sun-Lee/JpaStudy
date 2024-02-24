c540295e3c7bf2289a31307f23d7300a5ec7bbad
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-02-24 23:52:45 +0900

    [fix/도메인 설계] Album, Book, Movie 클래스에서 @DiscriminatorColumn -> @DiscriminatorValue로 변경, single table이므로 DB에서 구분 가능하게 하기위함

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -1,12 +1,13 @@
 package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@DiscriminatorColumn("B") // single table이므로 DB에서 구분 가능해야함. 기본으로 두면 Book으로 들어감.
+@DiscriminatorValue("B") // single table이므로 DB에서 구분 가능해야함. 기본으로 두면 Book으로 들어감.
 @Getter @Setter
 public class Book extends Item{
 
