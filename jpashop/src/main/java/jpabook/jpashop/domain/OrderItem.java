a8e5b078efb5825b0ac239aed3535e8efab0c09b
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-02-26 15:54:48 +0900

    [feat/주문 도메인] 주문, 주문상품 엔티티 개발 : 1) 주문 엔티티 - 생성 메서드(createOrder()), 주문 취소(cancel()), 전체 주문 가격 조회 2) 주문 상품 엔티티 - 생성 메서드(createOrderItem()), 주문 취소(cancel()), 주문 가격 조회(getTotalPrice()) -> 주문 상품 엔티티 메서드 먼저 실행 후 주문 엔티티 메서드 실행되는 방식

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 @Entity
 @Table(name = "order_item")
@@ -24,4 +25,24 @@ public class OrderItem {
 
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item); // setOrder는 Order 쪽에서 해주었기 때문에 안해줌
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);// 재고 수량 원복
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
