b9b80b90786ae93520559e5ec51bf5c359cb7004
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-03-01 01:40:33 +0900

    [feat/API 고급 - 컬렉션 조회 최적화] 주문 조회 V2.엔티티를 DTO로 변환 : (Order)Dto 안에도 엔티티(OrderItem)가 있으면 안됨 -> 엔티티의 스펙이 API에 모두 노출돼버리기 때문에 문제 발생

--- jpashop2/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop2/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.jaxb.SpringDataJaxb;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -26,5 +31,34 @@ public class OrderApiController {
             orderItems.stream().forEach(o -> o.getItem().getName());
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o)).toList();
+        return collect;
+    }
+
+    @Data
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItem> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+//            order.getOrderItems().stream().forEach(o -> o.getItem().getName()); // 프록시 객체 초기화
+            orderItems = order.getOrderItems();
+        }
     }
 }
