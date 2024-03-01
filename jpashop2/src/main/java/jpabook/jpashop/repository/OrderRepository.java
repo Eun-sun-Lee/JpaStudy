9c751f04fd0b5650d4e4b3cc3b35a810674fb72e
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-03-02 02:14:30 +0900

    [feat/QueryDSL] QueryDSL 소개 : build.gradle에 Querydsl을 위한 환경설정 및 QClass 추가, Querydsl을 활용하여 OrderRepository에 RequestParam으로 넘어온 orderStatus와 membername으로 필터링 조건을 걸어 주문 내역을 조회하는 findAll 함수 생성 / Querydsl은 SQL(JPQL)과 모양이 유사하면서 자바 코드로 동적 쿼리를 편리하게 생성 가능, 높은 개발 생산성과 동시에 쿼리 오류를 컴파일 시점에 빠르게 잡을 수 O

--- jpashop2/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop2/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,9 +1,14 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QOrder;
 import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -11,6 +16,9 @@ import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static jpabook.jpashop.domain.QMember.member;
+import static jpabook.jpashop.domain.QOrder.order;
 
 @Repository
 @RequiredArgsConstructor
@@ -134,31 +142,33 @@ public class OrderRepository {
                 .getResultList();
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//        QOrder order = QOrder.order;
-//        QMember member = QMember.member;
-//
-//        return query
-//                .select(order)
-//                .from(order)
-//                .join(order.member, member)
-//                .where(statusEq(orderSearch.getOrderStatus()),
-//                        nameLike(orderSearch.getMemberName()))
-//                .limit(1000)
-//                .fetch();
-//    }
-//
-//    private BooleanExpression statusEq(OrderStatus statusCond) {
-//        if (statusCond == null) {
-//            return null;
-//        }
-//        return order.status.eq(statusCond);
-//    }
-//
-//    private BooleanExpression nameLike(String nameCond) {
-//        if (!StringUtils.hasText(nameCond)) {
-//            return null;
-//        }
-//        return member.name.like(nameCond);
-//    }
+    public List<Order> findAll(OrderSearch orderSearch) {
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()),
+                        nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) {
+            return null;
+        }
+        return order.status.eq(statusCond);
+    }
+
+    private BooleanExpression nameLike(String nameCond) {
+        if (!StringUtils.hasText(nameCond)) {
+            return null;
+        }
+        return member.name.like(nameCond);
+    }
 }
