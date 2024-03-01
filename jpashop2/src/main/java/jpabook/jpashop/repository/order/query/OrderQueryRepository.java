f8d708a11a6047c7ad9879f5163d7217ec584414
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-03-01 22:47:36 +0900

    [feat/API 고급 - 컬렉션 조회 최적화] 주문 조회 V6.JPA에서 DTO로 직접 조회, 플랫 데이터 최적화 : Order - OrderItem - Item 조인하여 한번의 쿼리로 조회, 일대다 조인을 실행했기 때문에 Order 데이터가 중복으로 들어감, 애플리케이션에서 추가 작업이 큼(API 스펙에 맞춰 OrderFlatDto -> OrderQueryDto 변환 과정 필요), 페이징 불가능(우리가 원하는 형태의 중복 데이터가 없는 상태에서의 order를 기준으로 한 페이징 불가능)

--- jpashop2/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ jpashop2/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -38,7 +38,7 @@ public class OrderQueryRepository {
 
     private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
         List<OrderItemQueryDto> orderItems = em.createQuery(
-                        "se lect new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                        "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
                                 " from OrderItem oi" +
                                 " join oi.item i" +
                                 " where oi.order.id in :orderIds", OrderItemQueryDto.class) // In query로 변경
@@ -73,5 +73,16 @@ public class OrderQueryRepository {
                         " join o.member m" +
                         " join o.delivery d", OrderQueryDto.class
         ).getResultList();
+    }
+
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                "select new jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d" +
+                        " join o.orderItems oi" +
+                        " join oi.item i", OrderFlatDto.class)
+                .getResultList();
     }
 }
